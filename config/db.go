cf58a219540487f18e9d1c52cd3fbde1e7048f9e
Dihas Ananda <101080895+dihasananda@users.noreply.github.com>
2024-07-27 13:53:52 +0000

    feature: add replit configuration

--- config/db.go
+++ config/db.go
@@ -7,16 +7,16 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
 	dbURL := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open("postgres", dbURL)
